2c35efc9c3766a7c1f67328a3a2018d613c3a7d4
Tobin Yehle <tobinyehle@gmail.com>
2020-05-18 23:34:20 -0700

    produce better plots

--- firmware/colors.py
+++ firmware/colors.py
@@ -20,9 +20,16 @@ def read_data() -> pandas.DataFrame:
     # plt.yscale("log")
     # plt.show()
 
-    plt.plot(data["T"], data["R"], "r-",
-             data["T"], data["G"], "g-",
-             data["T"], data["B"], "b-")
+    plt.plot(data["T"], data["R"], "r", linestyle=(0, (8, 4)))
+    plt.plot(data["T"], data["G"], "g", linestyle=(0, (6, 2, 2, 2)))
+    plt.plot(data["T"], data["B"], "b", linestyle=(0, (2, 1)))
+    plt.title("Blackbody RGB Color")
+    plt.xticks([x * 1000 for x in range(11)])
+    plt.xlim(1000, 10000)
+    plt.xlabel("Temperature / K")
+    # plt.yticks([y*32 for y in range(9)])
+    plt.ylabel("Color Channel Value")
+    plt.legend(["Red", "Green", "Blue"])
     plt.show()
 
     sunrise.to_csv("sunrise.csv", index=False)
@@ -31,19 +38,35 @@ def read_data() -> pandas.DataFrame:
 
 
 def plot_brightness() -> None:
-    for k in [1/32, 1/16, 1/8, 1/4, 1/2, 1, 2, 4]:
+    for k, style, color in [
+        (8, (0, (2, 4)), "#00b2ff"),
+        (1, (0, (4, 2)), "#0084bd"),
+        (1 / 8, "solid", "#000000"),
+        (1 / 64, (0, (6, 2, 2, 2)), "#bd0084"),
+        (1 / 512, (0, (3, 4, 1, 4)), "#ff00b2"),
+    ]:
         ts = [i / 1000.0 for i in range(0, 1000)]
         bs = [brightness(k, t) for t in ts]
-        plt.plot(ts, bs, label="k = " + str(k))
+        p, q = k.as_integer_ratio()
+        label = f"k = {p}" if q == 1 else f"k = {p}/{q}"
+        plt.plot(ts, bs, label=label, linestyle=style, color=color)
+    plt.title("Exponential Brightness Function")
     plt.ylabel("Brightness")
     plt.xlabel("Time")
+    plt.xlim(0, 1)
+    plt.ylim(0, 1)
     plt.legend()
     plt.show()
 
 
+def brightness_ln(k: float, t: float) -> float:
+    u = math.exp(1 / k) - 1
+    return k * math.log(t + 1 / u) + k * math.log(u)
+
+
 def brightness(k: float, t: float) -> float:
-    u = math.exp(1/k) - 1
-    return k * math.log(t + 1/u) + k * math.log(u)
+    a = math.log(1 / k + 1)
+    return k * (math.exp(a * t) - 1)
 
 
 def main(data: pandas.DataFrame):
@@ -56,7 +79,7 @@ def main(data: pandas.DataFrame):
             if partial >= 1:
                 t += 1
                 partial -= 1
-            if t+1 >= len(data):
+            if t + 1 >= len(data):
                 partial = 1
                 t -= 1
                 done = True
@@ -68,34 +91,43 @@ def main(data: pandas.DataFrame):
 
     def color_between(low, high, t, fraction) -> Tuple[float, float, float]:
         def interpolate(name: str) -> float:
-            return low[name] + (high[name] - low[name])*fraction
+            return low[name] + (high[name] - low[name]) * fraction
 
         def scale(x: float) -> float:
             norm = (t + fraction) / (len(data) - 1)
-            return brightness(k=0.5, t=norm) * x
+            return brightness(k=0.125, t=norm) * x
 
-        return (scale(interpolate("R")),
-                scale(interpolate("G")),
-                scale(interpolate("B")))
+        return (
+            scale(interpolate("R")),
+            scale(interpolate("G")),
+            scale(interpolate("B")),
+        )
 
     def init():
         # print("init")
         ax.set_ylim(0, 10)
         ax.set_xlim(0, 10)
-        return box,
+        return (box,)
 
     fig, ax = plt.subplots()
-    box, = ax.fill([0, 0, 10, 10], [0, 10, 10, 0])
+    (box,) = ax.fill([0, 0, 10, 10], [0, 10, 10, 0])
 
     def run(data):
         # update the data
         r, g, b = data
         box.set_color((r, g, b))
-        return box,
+        return (box,)
 
-    a = animation.FuncAnimation(fig, run, data_gen, blit=True, interval=10,
-                                repeat=False, init_func=init)
-    plt.show(a)
+    a = animation.FuncAnimation(
+        fig,
+        run,
+        data_gen,
+        blit=True,
+        interval=10,
+        repeat=False,
+        init_func=init,
+    )
+    plt.show(block=a)
 
 
 if __name__ == "__main__":
