539ac93d5d1066e2f4b4d025dd641ba8e46a29c5
Tobin Yehle <tobinyehle@gmail.com>
2020-06-07 14:58:01 -0700

    fix the fonts on the hist plot

--- firmware/normalization/hist.py
+++ firmware/normalization/hist.py
@@ -4,6 +4,14 @@ import matplotlib
 import matplotlib.pyplot as plt
 import PIL.Image
 
+# plt.rcParams["font.family"] = "Source Serif Pro"
+plt.rcParams["font.family"] = "Open Sans"
+title_font_settings = {
+    "fontname": "Open Sans Condensed",
+    "fontweight": "bold",
+    "fontsize": 16,
+}
+
 
 def plot_image_hist(filepath: str) -> None:
     hist = PIL.Image.open(filepath).histogram()
@@ -29,11 +37,11 @@ def plot_image_hist(filepath: str) -> None:
     fig, axes = plt.subplots(
         3, sharey=True, sharex=True, gridspec_kw={"hspace": 0}
     )
-    fig.suptitle("White Color Histogram")
+    fig.suptitle("White Color Histogram", **title_font_settings)
     plot_channel(r, axes[0], color="r", label="Red")
     plot_channel(g, axes[1], color="g", label="Green")
     plot_channel(b, axes[2], color="b", label="Blue")
-    x_ticks = [0] + [(x+1)*32 - 1 for x in range(8)]
+    x_ticks = [0] + [(x + 1) * 32 - 1 for x in range(8)]
     axes[2].set_xticks(x_ticks)
     axes[2].set_xticklabels([f"{x:02X}" for x in x_ticks])
     axes[1].set_ylabel("Count")
